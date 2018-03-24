b4a584a28a6f67d2db563a5a1f57afac716f0a94
huwenjian <hwj_gz@163.com>
2018-03-24 22:54:27 +0800

    add todo in hm-server

--- hm-server/src/main/java/com/hm/controller/QueryEsController.java
+++ hm-server/src/main/java/com/hm/controller/QueryEsController.java
@@ -1,13 +1,24 @@
 package com.hm.controller;
 
 
+import com.hm.dto.HmUser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/query")
 public class QueryEsController {
 
+    //@todo when query, new HttpConnectionManager
 
+    @GetMapping
+    private List<HmUser> getUser(HttpServletRequest request) {
+
+        return null;
+    }
 
 }
