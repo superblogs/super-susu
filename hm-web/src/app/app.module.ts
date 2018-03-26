035a982ccb8a9970dc57d0a8ae9dd7f243a117da
huwenjian <hwj_gz@163.com>
2018-03-26 11:13:50 +0800

    start a search page

--- hm-web/src/app/app.module.ts
+++ hm-web/src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ROUTES} from './app.routes';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './component/login/login.component';
 import {LoginService} from './service/login.service';
 import {HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './component/home/home.component';
+import {HomeComponent} from './component/home/home.component';
 import {FormsModule} from '@angular/forms';
 import {AuthGuard} from './service/AuthGuard';
-import { HeaderComponent } from './page/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
