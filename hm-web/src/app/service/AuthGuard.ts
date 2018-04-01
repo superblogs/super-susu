e1ef2c95398ed0c4df1821b1fa4fbb628c4f87b4
huwenjian <hwj_gz@163.com>
2018-04-01 19:47:17 +0800

    AuthGuard <list>

--- hm-web/src/app/service/AuthGuard.ts
+++ hm-web/src/app/service/AuthGuard.ts
@@ -9,6 +9,12 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    //AuthGuard <List >
+    var path = route.routeConfig.path;
+    console.log(path);
+    const nextRoute = ['home', 'good-list', 'good-detail', 'cart', 'profile'];
+
     console.log(sessionStorage.getItem('loginStatus'));
     if (!sessionStorage.getItem('loginStatus')) {
       this.route.navigateByUrl('/');
