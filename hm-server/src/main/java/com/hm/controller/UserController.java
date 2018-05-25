f79429758a6efafd40d5e2133dcca465172b13c5
huwenjian <hwj_gz@163.com>
2018-05-25 17:59:21 +0800

    test the hashMap

--- hm-server/src/main/java/com/hm/controller/UserController.java
+++ hm-server/src/main/java/com/hm/controller/UserController.java
@@ -3,12 +3,9 @@ package com.hm.controller;
 import com.hm.dto.HmUser;
 import com.hm.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
 import java.util.List;
 
 @RestController
