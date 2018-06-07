c405fe8fb5692a7f8cfef339c1ccf8d54b11065b
huwenjian <hwj_gz@163.com>
2018-06-07 15:04:29 +0800

    #1 : word mapper

--- common-core/src/main/java/com/hm/yewu/wordmap/WordMapController.java
+++ common-core/src/main/java/com/hm/yewu/wordmap/WordMapController.java
@@ -1,4 +1,21 @@
 package com.hm.yewu.wordmap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class WordMapController {
+    @Autowired
+    private WordMapRepository wordMapRepository;
+
+    @GetMapping(value = "word")
+    public List<WordMapDto> getWordMapperList() {
+
+        return wordMapRepository.findAll();
+    }
 }
