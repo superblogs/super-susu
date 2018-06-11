df77b4d0f08742d159e87d80ad1d94edb889caae
huwenjian <hwj_gz@163.com>
2018-06-11 16:06:22 +0800

    intStream Test

--- hm-server/src/test/java/com/hm/studyjdk/IntStreamStudy.java
+++ hm-server/src/test/java/com/hm/studyjdk/IntStreamStudy.java
@@ -37,4 +37,12 @@ public class IntStreamStudy {
                 ).forEach(t -> System.out.println("符合勾股定理的三角形有：" + t[0] + " " + t[1] + " " + t[2]));
     }
 
+    @Test
+    public void createStream() {
+        Stream.iterate(0, i -> i + 2).limit(10).forEach(System.out::println);
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(20)
+                .forEach(t -> System.out.println(t[0] + t[1]));
+        Stream.generate(Math::random).limit(10).forEach(i-> System.out.println(i*100));
+    }
 }
